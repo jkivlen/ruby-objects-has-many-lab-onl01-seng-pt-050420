5621b2768d076c931c2f9e98e37028614c4116ba
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-06-09 16:03:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,15 +10,4 @@ class Song
     @@all << self
   end
   
-  def artist_name
-    if self.artist
-      self.artist.name
-    else
-      nil
-    end
-  end
-
-  def self.all
-    @@all
-  end
 end
