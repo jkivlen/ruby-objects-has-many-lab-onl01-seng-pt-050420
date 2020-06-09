9c604a645a042c1fdb9ce200efe4c29e4b671f4d
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-06-09 16:27:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,11 @@
 class Artist
   
-  attr_accessor :name 
+  attr_accessor :name, :songs 
   
   def initialize(name)
          @name = name
-         @songs = []
        end
        
-      def add_song(song)
-        @songs << song
-        song.artist = self
-      end
       
       def songs
         @songs
